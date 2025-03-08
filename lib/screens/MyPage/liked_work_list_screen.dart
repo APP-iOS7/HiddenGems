b3bd0ad1b27400a8e25b2095407db39d4d70dad7
kdy <kokbael@naver.com>
2025-03-08 17:01:45 +0900

    LikedWorkListScreen 쿼리 수정

--- lib/screens/MyPage/liked_work_list_screen.dart
+++ lib/screens/MyPage/liked_work_list_screen.dart
@@ -8,46 +8,38 @@ import 'package:provider/provider.dart';
 class LikedWorkListScreen extends StatelessWidget {
   const LikedWorkListScreen({super.key});
 
-  Future<List<Work>> fetchLikedWorks(List<String> likedWorkIds) async {
-    List<Work> likedWorks = [];
+  Future<List<String>> fetchLikedWorkIds(String userId) async {
     final firestore = FirebaseFirestore.instance;
+    DocumentSnapshot userDoc =
+        await firestore.collection('users').doc(userId).get();
 
-    // Firestore의 whereIn 연산자는 한 번의 쿼리에서 최대 10개의 값만 처리할 수 있음
-    for (var i = 0; i < likedWorkIds.length; i += 10) {
-      var end = (i + 10 < likedWorkIds.length) ? i + 10 : likedWorkIds.length;
-      var chunk = likedWorkIds.sublist(i, end);
-
-      var querySnapshot = await firestore
-          .collection('works')
-          .where(FieldPath.documentId, whereIn: chunk)
-          .get();
-
-      var works =
-          querySnapshot.docs.map((doc) => Work.fromFirestore(doc)).toList();
-      likedWorks.addAll(works);
+    if (userDoc.exists) {
+      Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+      List<String> likedWorkIds =
+          List<String>.from(userData['likedWorks'] ?? []);
+      return likedWorkIds;
+    } else {
+      return [];
     }
-
-    return likedWorks;
   }
 
   @override
   Widget build(BuildContext context) {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
-    final likedWorkIds = userProvider.user?.likedWorks ?? [];
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('내가 좋아요한 작품'),
+        title: const Text('내가 좋아요한 작품'),
       ),
-      body: FutureBuilder<List<Work>>(
-        future: fetchLikedWorks(likedWorkIds),
+      body: FutureBuilder<List<String>>(
+        future: fetchLikedWorkIds(userProvider.user!.id),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text('오류가 발생했습니다.'));
+            return const Center(child: Text('오류가 발생했습니다.'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(
+            return const Center(
                 child: Text('좋아요한 작품이 없습니다.', style: TextStyle(fontSize: 18)));
           } else {
             var likedWorks = snapshot.data!;
@@ -62,31 +54,54 @@ class LikedWorkListScreen extends StatelessWidget {
                 ),
                 itemCount: likedWorks.length,
                 itemBuilder: (context, index) {
-                  final work = likedWorks[index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => WorkdetailScreen(work: work),
+                  final workId = likedWorks[index];
+
+                  // 각 workId에 대해 Firestore에서 작업 상세 정보를 가져옴
+                  return FutureBuilder<DocumentSnapshot>(
+                    future: FirebaseFirestore.instance
+                        .collection('works')
+                        .doc(workId)
+                        .get(),
+                    builder: (context, workSnapshot) {
+                      if (workSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      if (workSnapshot.hasError ||
+                          !workSnapshot.hasData ||
+                          !workSnapshot.data!.exists) {
+                        return const Center(child: Text('오류가 발생했습니다.'));
+                      }
+
+                      // Firestore 문서를 Work 객체로 변환
+                      final work = Work.fromFirestore(workSnapshot.data!);
+
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  WorkdetailScreen(work: work),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                work.workPhotoURL.isNotEmpty
+                                    ? work.workPhotoURL
+                                    : 'https://picsum.photos/200/300',
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
                       );
                     },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            work.workPhotoURL.isNotEmpty
-                                ? work.workPhotoURL
-                                : 'https://picsum.photos/200/300',
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      // 이미지 위에 오버레이나 텍스트를 추가하려면 여기에 작성
-                    ),
                   );
                 },
               ),
