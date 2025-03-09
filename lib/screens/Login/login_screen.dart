7a83caa7253a3effb0030ae3da5093a0ca67142e
kdy <kokbael@naver.com>
2025-03-09 22:37:06 +0900

    프로필 업데이트 페이지 수정, 로그인 페이지들FocusScope.of(context).unfocus(); 추가

--- lib/screens/Login/login_screen.dart
+++ lib/screens/Login/login_screen.dart
@@ -12,109 +12,114 @@ class Loginscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Spacer(),
-              Text(
-                'Hidden Gems',
-                style: TextStyle(fontWeight: FontWeight.w700),
-              ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: _emailController,
-                      decoration: const InputDecoration(labelText: '이메일'),
-                    ),
-                    SizedBox(height: 20),
-                    TextField(
-                      obscureText: true,
-                      controller: _passwordController,
-                      decoration: const InputDecoration(labelText: '비밀번호'),
-                    ),
-                  ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Spacer(),
+                Text(
+                  'Hidden Gems',
+                  style: TextStyle(fontWeight: FontWeight.w700),
                 ),
-              ),
-              SizedBox(height: 60),
-              InkWell(
-                  onTap: signIn,
-                  borderRadius: BorderRadius.circular(8),
-                  child: Container(
-                    width: 330,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF9800CB).withValues(alpha: 0.65),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.account_circle,
-                          color: Colors.white,
-                          size: 28,
-                        ),
-                        // 오른쪽 텍스트
-                        SizedBox(
-                          width: 280,
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              "이메일/비밀번호로 로그인",
-                              style: TextStyle(
-                                fontSize: 17,
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(labelText: '이메일'),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        obscureText: true,
+                        controller: _passwordController,
+                        decoration: const InputDecoration(labelText: '비밀번호'),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 60),
+                InkWell(
+                    onTap: signIn,
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      width: 330,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF9800CB).withValues(alpha: 0.65),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.account_circle,
+                            color: Colors.white,
+                            size: 28,
+                          ),
+                          // 오른쪽 텍스트
+                          SizedBox(
+                            width: 280,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "이메일/비밀번호로 로그인",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    )),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: loginWithKakao,
+                      child: Image.asset(
+                        'lib/assets/kakaoLogin.png',
+                        width: 160,
+                      ),
                     ),
-                  )),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                    onTap: loginWithKakao,
-                    child: Image.asset(
-                      'lib/assets/kakaoLogin.png',
-                      width: 160,
+                    SizedBox(width: 10),
+                    InkWell(
+                      onTap: loginWithGoogle,
+                      child: Image.asset(
+                        'lib/assets/googleLogin.png',
+                        width: 160,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 10),
-                  InkWell(
-                    onTap: loginWithGoogle,
-                    child: Image.asset(
-                      'lib/assets/googleLogin.png',
-                      width: 160,
-                    ),
-                  ),
-                ],
-              ),
-              TextButton(
-                onPressed: () async {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => SignUpScreen()));
-                },
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey.shade700,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.grey),
+                  ],
                 ),
-              ),
-              Spacer(),
-            ],
+                TextButton(
+                  onPressed: () async {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => SignUpScreen()));
+                  },
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey.shade700,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.grey),
+                  ),
+                ),
+                Spacer(),
+              ],
+            ),
           ),
         ),
       ),
