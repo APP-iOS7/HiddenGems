e3ce1580a27f7e5e816f91d711e357bd39ef63c3
kdy <kokbael@naver.com>
2025-03-10 10:59:01 +0900

    홈 - 인기 작품들 UI 수정

--- lib/widgets/popular_works.dart
+++ lib/widgets/popular_works.dart
@@ -28,7 +28,9 @@ class PopularWorks extends StatelessWidget {
 
         return SizedBox(
           height: 160,
-          child: ListView.builder(
+          child: ListView.separated(
+            separatorBuilder: (BuildContext context, int index) =>
+                const SizedBox(width: 10),
             scrollDirection: Axis.horizontal,
             itemCount: works.length,
             itemBuilder: (context, index) {
@@ -46,12 +48,24 @@ class PopularWorks extends StatelessWidget {
                       SizedBox(
                         height: 90,
                         child: Card(
-                          child: Image.network(
-                            work.workPhotoURL,
-                            fit: BoxFit.cover,
+                          child: Container(
+                            decoration: BoxDecoration(boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withValues(alpha: 0.7),
+                                spreadRadius: 8,
+                                blurRadius: 10,
+                                offset:
+                                    Offset(0, 5), // changes position of shadow
+                              )
+                            ]),
+                            child: Image.network(
+                              work.workPhotoURL,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
+                      SizedBox(height: 10),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 4.0),
                         child: Row(
@@ -62,8 +76,9 @@ class PopularWorks extends StatelessWidget {
                               width: 70,
                               child: Text(
                                 work.title,
-                                style: TextStyle(fontSize: 18),
-                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w700),
+                                overflow: TextOverflow.fade,
                               ),
                             ),
                             Column(
@@ -71,11 +86,11 @@ class PopularWorks extends StatelessWidget {
                               children: [
                                 Text(
                                   work.artistNickName,
-                                  style: TextStyle(fontSize: 9),
+                                  style: TextStyle(fontSize: 14),
                                 ),
                                 Text(
                                   '${NumberFormat('###,###,###,###').format(work.minPrice)} 원',
-                                  style: TextStyle(fontSize: 9),
+                                  style: TextStyle(fontSize: 10),
                                 ),
                               ],
                             ),
